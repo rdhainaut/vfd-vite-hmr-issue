95583c4b3b54ea2a77852162276a3072bb735911
Raphael Dhainaut <raphaeldhainaut@outlook.com>
2023-05-26 12:18:34 +0200

    initial commit with HMR issue

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <HelloWorld msg="You did it!" />
+</template>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-facing-decorator/dist/index-return-cons' // HMR doesn't work with this
+//import { Component, Prop, Vue } from 'vue-facing-decorator' // HMR work with this
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() msg!: string
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div>
+      <!-- Edit this lorem ipsum content then save to see HRM in live (it should update the view but ... it doesn't) -->
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. 11
+    </div>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
+  </div>
+</template>
